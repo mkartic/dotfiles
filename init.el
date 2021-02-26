274030c14d68ab2707e8483ca00db9154a968f2d
M Karthick <mkartic89@yahoo.com>
2021-02-26 20:25:59 +0530

    Including init.el for emacs config. Hopefully will move to org in the future. * i3/.config/i3/config:

--- init.el
+++ init.el
@@ -11,8 +11,6 @@
 
 (set-face-attribute 'default nil :font "Fira Code" :height 140)
 
-(load-theme 'wombat)
-
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Initialize package sources
@@ -87,7 +85,8 @@
   :ensure t
   :init (doom-modeline-mode 1)
   :custom (doom-modeline-height 1))
-;; (use-package doom-themes)
+(use-package doom-themes)
+(load-theme 'doom-dracula t)
 
 (use-package all-the-icons)
 
@@ -163,4 +162,109 @@
 (rune/leader-keys
   "ts" '(hyrda-text-scale/body :which-key "scale text"))
 
-(use-package doom-themes)
+(use-package projectile
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/code")
+    (setq projectile-project-search-path '("~/code" "~/dotfiles")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil-magit
+  :after magit)
+
+(use-package virtualenvwrapper
+  :custom (venv-location
+	   '(
+	     "~/code/k-venv/"
+	     "~/code/reef-task/")
+	   eshell-prompt-function
+	   (lambda ()
+	     (concat venv-current-name " $ ")
+	     ))
+  :config
+  (venv-initialize-eshell) ;; if you want eshell support  
+  )
+(venv-initialize-interactive-shells) ;; if you want interactive shell support
+;; For some reason, trying to load a notebook is making emacs shoot up to 100% memory usage and crashing. Debugging later maybe. 
+;; (use-package ein)
+
+
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun efs/org-mode-visual-fill ()
+  (setq visual-fill-column-width 100
+        visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :hook (org-mode . efs/org-mode-visual-fill))
+
+(use-package go-mode)
+(use-package i3wm-config-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/organized/interview_prep.org"))
+ '(package-selected-packages
+   '(xclip w3m i3wm-config-mode go-mode visual-fill-column org-bullets evil-magit magit counsel-projectile projectile which-key virtualenvwrapper use-package rainbow-delimiters org-pomodoro ivy-rich hydra helpful general evil-collection ein doom-themes doom-modeline counsel))
+ '(safe-local-variable-values '((projectile-project-run-cmd . "go run pe6.go"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
